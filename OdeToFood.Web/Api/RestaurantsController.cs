3541e3bdb70b1b35628ed016155601cb5cef4551
KatarinaSundqvist <singsongkat@live.com>
2019-12-31 16:24:33 +0100

    Up to and including chapter 7

--- OdeToFood.Web/Api/RestaurantsController.cs
+++ OdeToFood.Web/Api/RestaurantsController.cs
@@ -1,14 +1,9 @@
 ﻿using OdeToFood.Data.Models;
 using OdeToFood.Data.Services;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
-namespace OdeToFood.Web.Api
-{
+namespace OdeToFood.Web.Api {
     public class RestaurantsController : ApiController {
         private readonly IRestaurantData db;
 
--- OdeToFood.Web/Controllers/RestaurantsController.cs
+++ OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -38,9 +38,45 @@ namespace OdeToFood.Web.Controllers {
 
             if (ModelState.IsValid) {
                 db.Add(restaurant);
-                return View(); 
+                return RedirectToAction("Details", new { id = restaurant.Id });
             }
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id) {
+            var model = db.Get(id);
+            if (model == null) {
+                return View("NotFound");
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant) {
+            if (ModelState.IsValid) {
+                db.Update(restaurant);
+                TempData["Message"] = "You have saved the restaurant!";
+                return RedirectToAction("Details", new { id = restaurant.Id });
+            }
+            return View(restaurant);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id) {
+            var model = db.Get(id);
+            if (model == null) {
+                return View("NotFound");
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, FormCollection form) {
+            db.Delete(id);
+            return RedirectToAction("Index");
         }
     }
 }
